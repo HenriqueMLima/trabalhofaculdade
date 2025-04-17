bdbd63df1416d00119d9db252d7ea212b174a189
henriqueMlima <henriquegamadf21@gmail.com>
2025-04-17 16:41:14 -0300

    teste

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app.secret_key = 'qCHoCA0U1IOgBOqZFCCZ9GJt3ce8aZnS'
 CORS(app)
 CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})
 
-# Configurações de conexão
 db_config = {
     'host': 'mysql-d30b100-gustavocnc.d.aivencloud.com',
     'user': 'avnadmin',
@@ -61,7 +60,6 @@ def user_login():
             if user is None:
                 return jsonify({"message": "Usuário não encontrado"}), 404
             
-            # Tenta acessar 'Id_usuario' e, se não existir, tenta 'id_usuario'
             user_id = user.get('ID_Usuario') or user.get('id_usuario')
             
             if bcrypt.checkpw(senha.encode('utf-8'), user['Senha'].encode('utf-8')):
@@ -85,7 +83,6 @@ def register():
     """Cadastra um novo usuário no sistema."""
     user_data = request.json
     
-    # Validação de entrada
     if not user_data or not user_data.get('nome') or not user_data.get('email') or not user_data.get('senha'):
         return jsonify({"message": "Nome, email e senha são obrigatórios"}), 400
     
@@ -93,7 +90,6 @@ def register():
     email = user_data['email']
     senha = user_data['senha']
     
-    # Hash da senha com bcrypt
     hashed_senha = bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt())
     
     try:
@@ -101,14 +97,13 @@ def register():
         if connection:
             cursor = connection.cursor()
             
-            # Verifica se o usuário já existe
+
             cursor.execute("SELECT * FROM Usuario WHERE Email = %s", (email,))
             user = cursor.fetchone()
             
             if user:
                 return jsonify({"message": "Usuário já existe com esse email"}), 409
             
-            # Insere o novo usuário no banco de dados
             insert_query = """
                 INSERT INTO Usuario (Nome, Email, Senha) 
                 VALUES (%s, %s, %s)
@@ -129,17 +124,14 @@ def register():
             
 @app.route('/projeto', methods=['POST'])
 def cadastrar_projeto():
-    # Verificar se o usuário está autenticado
     if 'user_id' not in session:
         return jsonify({"message": "Você precisa estar autenticado para cadastrar um projeto"}), 401
 
-    # Receber os dados do projeto do cliente
     projeto_data = request.json
     nome = projeto_data.get('nome')
     descricao = projeto_data.get('descricao')
     id_categoria = projeto_data.get('id_categoria')  
 
-    # Verificar se todos os campos necessários foram fornecidos
     if not nome or not descricao:
         return jsonify({"message": "Nome e descrição são obrigatórios"}), 400
 
@@ -148,7 +140,6 @@ def cadastrar_projeto():
         if connection:
             cursor = connection.cursor()
             
-            # Inserir o projeto no banco de dados associado ao usuário logado
             insert_query = """
                 INSERT INTO Projeto (Nome, Descricao, ID_Categoria, Id_usuario)
                 VALUES (%s, %s, %s, %s)
@@ -175,7 +166,6 @@ def listar_projetos():
         if connection:
             cursor = connection.cursor(dictionary=True)
             
-            # Seleciona todos os projetos no banco de dados
             select_query = "SELECT * FROM Projeto"
             cursor.execute(select_query)
             projetos = cursor.fetchall()
